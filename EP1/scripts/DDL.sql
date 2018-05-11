8defc7cb3339558ff57c470171b240ca312bf6c1
Leonardo Lana <leolanavo@gmail.com>
2018-05-11 18:00:37 -0300

    [EP1] Add the IF NOT EXISTS to all CREATE TABLE
    Signed-off-by: Leonardo Lana <leolanavo@gmail.com>

--- EP1/scripts/DDL.sql
+++ EP1/scripts/DDL.sql
@@ -2,41 +2,41 @@ CREATE DATABASE yggdrasil_bia_lana;
 \c yggdrasil_bia_lana;
 
 
-CREATE TABLE USUARIOS(
+CREATE TABLE IF NOT EXISTS USUARIO(
     US_ID               SERIAL PRIMARY KEY     NOT NULL,
     US_LOGIN            CHAR(320)              NOT NULL,
     US_PASSWORD         CHAR(12)               NOT NULL
 );
 
-CREATE TABLE ALUNO(
+CREATE TABLE IF NOT EXISTS ALUNO(
     AL_ID               SERIAL PRIMARY KEY     NOT NULL,
     AL_NUSP             INT                    NOT NULL UNIQUE,
     AL_NOME             CHAR(50)               NOT NULL,
     AL_DATA_MATRICULA   CHAR(10)               NOT NULL,
     AL_DATA_FORMATURA   CHAR(10),
-    AL_USER_ID          INT                    references USUARIOS(US_ID)
+    AL_USER_ID          INT                    references USUARIO(US_ID)
 );
 
-CREATE TABLE PROFESSOR(
+CREATE TABLE IF NOT EXISTS PROFESSOR(
     PR_ID               SERIAL PRIMARY KEY     NOT NULL,
     PR_NUSP             INT                    NOT NULL UNIQUE,
     PR_NOME             CHAR(50)               NOT NULL,
     PR_AREA             CHAR(50)               NOT NULL,
-    PR_USER_ID          INT                    references USUARIOS(US_ID)
+    PR_USER_ID          INT                    references USUARIO(US_ID)
 );
 
-CREATE TABLE PERFIL(
+CREATE TABLE IF NOT EXISTS PERFIL(
     PE_ID               SERIAL PRIMARY KEY     NOT NULL,
     PE_NOME             CHAR(50)               NOT NULL UNIQUE
 );
 
-CREATE TABLE SERVICO(
+CREATE TABLE IF NOT EXISTS SERVICO(
     SR_ID               SERIAL PRIMARY KEY     NOT NULL,
     SR_NOME             CHAR(50)               NOT NULL UNIQUE,
     SR_DESCRICAO        TEXT                   NOT NULL
 );
 
-CREATE TABLE DISCIPLINA(
+CREATE TABLE IF NOT EXISTS DISCIPLINA(
     DS_ID                  SERIAL PRIMARY KEY     NOT NULL,
     DS_CODIGO              CHAR(7)                NOT NULL UNIQUE,
     DS_NOME                CHAR(50)               NOT NULL,
@@ -48,28 +48,27 @@ CREATE TABLE DISCIPLINA(
     DS_APROVEITAMENTO      INT                    NOT NULL, CHECK(DS_APROVEITAMENTO >= 0 AND DS_APROVEITAMENTO <= 2)
 );
 
-CREATE TABLE MODULO(
+CREATE TABLE IF NOT EXISTS MODULO(
     MD_ID                  SERIAL PRIMARY KEY     NOT NULL,
-    MD_NOME                CHAR(50)               NOT NULL UNIQUE,
+    MD_NOME                CHAR(50)               NOT NULL UNIQUE
 );
 
-CREATE TABLE TRILHA(
+CREATE TABLE IF NOT EXISTS TRILHA(
     TR_ID                  SERIAL PRIMARY KEY     NOT NULL,
-    TR_NOME                CHAR(50)               NOT NULL UNIQUE,
+    TR_NOME                CHAR(50)               NOT NULL UNIQUE
 );
 
-CREATE TABLE PERFIL_USA_SERVICO(
+CREATE TABLE IF NOT EXISTS PERFIL_USA_SERVICO(
     PERFIL_ID              INT    references PERFIL(PE_ID),
     SERVICO_ID             INT    references SERVICO(SR_ID)
 );
 
-CREATE TABLE OPTATIVA_FORMA_MODULO(
+CREATE TABLE IF NOT EXISTS OPTATIVA_FORMA_MODULO(
     OPTATIVA_ID         INT    references DISCIPLINA(DS_ID),
     MODULO_ID           INT    references MODULO(MD_ID)
 );
 
-
-CREATE TABLE ALUNO_CURSA_DISCIPLINA(
+CREATE TABLE IF NOT EXISTS ALUNO_CURSA_DISCIPLINA(
     ALUNO_ID            INT    references ALUNO(AL_ID),
     DISCIPLINA_ID       INT    references DISCIPLINA(DS_ID),
     PRETENDE_SEM        INT    NOT NULL CHECK(PRETENDE_SEM >= 1 AND PRETENDE_SEM <= 2),
@@ -77,15 +76,15 @@ CREATE TABLE ALUNO_CURSA_DISCIPLINA(
     STATUS              BOOLEAN
 );
 
-CREATE TABLE USUARIO_TEM_PERFIL(
+CREATE TABLE IF NOT EXISTS USUARIO_TEM_PERFIL(
     USUARIO_ID          INT    references USUARIO(US_ID),
     PERFIL_ID           INT    references PERFIL(PE_ID)
 );
-CREATE TABLE MODULO_PERTENCE_TRILHA(
+CREATE TABLE IF NOT EXISTS MODULO_PERTENCE_TRILHA(
     MODULO_ID           INT    references MODULO(MD_ID),
     TRILHA_ID           INT    references TRILHA(TR_ID)
 );
-CREATE TABLE PROFESSOR_TUTORA_ALUNO(
+CREATE TABLE IF NOT EXISTS PROFESSOR_TUTORA_ALUNO(
     PROFESSOR_ID        INT   references PROFESSOR(PR_ID),
     ALUNO_ID            INT   references ALUNO(AL_ID)
 );
